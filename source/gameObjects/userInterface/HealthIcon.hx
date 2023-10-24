16bcdcfc2c529c86da9dad4d40e3d0a414fa38a4
FAVIFANBULIDDEV <147041324+FAVIFANBULIDDEV@users.noreply.github.com>
2023-10-24 12:30:46 +0800

    Initial commit

---
+++ source/gameObjects/userInterface/HealthIcon.hx
@@ -0,0 +1,63 @@
+package gameObjects.userInterface;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.graphics.FlxGraphic;
+import sys.FileSystem;
+
+using StringTools;
+
+class HealthIcon extends FlxSprite
+{
+	// rewrite using da new icon system as ninjamuffin would say it
+	public var sprTracker:FlxSprite;
+	public var initialWidth:Float = 0;
+	public var initialHeight:Float = 0;
+
+	var splitThing:Int = 2;
+
+	public function new(char:String = 'bf', isPlayer:Bool = false)
+	{
+		super();
+		updateIcon(char, isPlayer);
+	}
+
+	public function updateIcon(char:String = 'bf', isPlayer:Bool = false)
+	{
+		var trimmedCharacter:String = char;
+		if (trimmedCharacter.contains('-'))
+			trimmedCharacter = trimmedCharacter.substring(0, trimmedCharacter.indexOf('-'));
+
+		var iconPath = char;
+		if (!FileSystem.exists(Paths.getPath('images/characters/icons/' + iconPath + '.png', IMAGE)))
+		{
+			if (iconPath != trimmedCharacter)
+				iconPath = trimmedCharacter;
+			else
+				iconPath = 'face';
+			trace('$char icon trying $iconPath instead you fuck');
+		}
+
+		antialiasing = true;
+		var iconGraphic:FlxGraphic = Paths.image('characters/icons/' + iconPath);
+		loadGraphic(iconGraphic); // load this first
+
+		splitThing = width/3 == height ? 3 : 2; // winning icon detection
+		loadGraphic(iconGraphic, true, Std.int(iconGraphic.width / splitThing), iconGraphic.height);
+
+		initialWidth = width;
+		initialHeight = height;
+
+		animation.add('icon', [0, 1, (splitThing == 3 ? 2 : 0)], 0, false, isPlayer);
+		animation.play('icon');
+		scrollFactor.set();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (sprTracker != null)
+			setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 30);
+	}
+}
